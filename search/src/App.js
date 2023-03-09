526f4b52d82408deab1babf462cff340215d3754
jyllsarta <illstarred.dive@gmail.com>
2023-03-10 00:27:47 +0900

    画像表示まで

--- search/src/App.js
+++ search/src/App.js
@@ -1,13 +1,36 @@
-import {useState} from "react";
+import {useLayoutEffect, useState} from "react";
 import './App.css';
 
 function App() {
   const [images, setImages] = useState([]);
+  const MIN_NUM=3;
+  const [stage, setStage] = useState(0);
+  const [number, setNumber] = useState(MIN_NUM);
+  const [title, setTitle] = useState("スタート");
+
+  useLayoutEffect(()=>{
+    if(title !== "スタート"){
+      addImage(null, stage, number);
+    }
+  }, [stage, number]);
+
+  const start = (e) => {
+    setTitle("画像検索ワード当てクイズ");
+    addImage(null, stage, number);
+  }
+
+  const addImage = (e, stg, num) => {
+    createImage();
+  }
+
+  function createImage(json) {
+    setImages(["https://webgl.vexil.jp/images/webgl.jpg"]);
+  }
 
   return (
     <div className="App">
       <header>
-        <h1>画像検索ワード</h1>
+        <h1 onClick={(e)=> start(e)}>画像検索ワード</h1>
       </header>
       <div>
         {images.map((src, i)=><img src={src} key={i} alt="問題"/>)}
