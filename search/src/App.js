3a5a73d35aba44c6d190c17789890ffd7ea6843c
jyllsarta <illstarred.dive@gmail.com>
2023-03-10 00:21:09 +0900

    CSSあてた

--- search/src/App.js
+++ search/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import {useState} from "react";
 import './App.css';
 
 function App() {
+  const [images, setImages] = useState([]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>画像検索ワード</h1>
       </header>
+      <div>
+        {images.map((src, i)=><img src={src} key={i} alt="問題"/>)}
+      </div>
+
+      <p>
+        <a href="https://pixabay.com" target="_blank" rel="noreferrer">
+          <img src="https://pixabay.com/static/img/logo.svg" alt="pixabay" />
+        </a>
+      </p>
     </div>
   );
 }
